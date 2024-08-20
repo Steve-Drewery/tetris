29be9511cbce674b9cebc7914c637396ce903315
Steve_Drewery <11940@coderacademy.edu.au>
2024-08-20 17:22:06 +1000

    Setup

---
+++ src/Main/Main.java
@@ -0,0 +1,21 @@
+package Main;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Simple Tetris");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+
+        // Add Main.GamePanel to window
+        GamePanel gp = new GamePanel();
+        window.add(gp);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gp.launchGame();
+    }
+}
